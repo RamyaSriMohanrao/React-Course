5204cdd2ed1a1f88b39e4cf4ceee42ee70ce7924
RamyaSriMohanrao <ramyasrimohanrao@gmail.com>
2025-02-08 13:03:42 +0530

    Updates

--- src/components/Grocery.js
+++
@@ -1,7 +0,0 @@
-// const Grocery = () => {
-//     return (
-//         <h1>This is Grocery online store and we have lot of child components inside this web page!!</h1>
-//     );
-// };
-
-// export default Grocery;
--- src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-
-export const LOGO_URL = "img\Good_Food.jpg" ; 
-// export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0lBTc9UoD5uXPRxvhxKeQXi9459RqFRoRuIFxBfC_PVqExLq76K4JO6wh6GOzqh_ofiI&usqp=CAU" ; 
-
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=/" ;
